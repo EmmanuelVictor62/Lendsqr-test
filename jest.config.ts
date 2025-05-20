378edcbdb5921bdef11b170be5d5f9fac2df3295
royalManuel <104584134+EmmanuelVictor62@users.noreply.github.com>
2025-05-20 04:07:33 +0100

    chore: Set up jest in project

--- jest.config.ts
+++ jest.config.ts
@@ -1,16 +1,21 @@
-module.exports = {
+import type { Config } from "jest";
+
+const config: Config = {
   preset: "ts-jest",
-  testEnvironment: "jest-environment-jsdom",
-  transform: {
-    "^.+\\.tsx?$": "babel-jest",
-  },
+  testEnvironment: "jsdom",
+  setupFilesAfterEnv: ["<rootDir>/jest.setup.ts"],
   moduleNameMapper: {
+    "\\.(css|scss)$": "identity-obj-proxy",
+    "^.+\\.svg$": "<rootDir>/__mocks__/svgMock.tsx",
     "^@/(.*)$": "<rootDir>/$1",
-    "^.+\\.(css|scss)$": "identity-obj-proxy",
+    "^constants/(.*)$": "<rootDir>/constants/$1",
+    "\\.svg$": "<rootDir>/__mocks__/svgrMock.tsx",
   },
-  setupFilesAfterEnv: ["<rootDir>/jest.setup.tsx"],
-  moduleFileExtensions: ["ts", "tsx", "js", "jsx"],
-  transformIgnorePatterns: [
-    "/node_modules/", // Ignore node_modules
-  ],
+  moduleFileExtensions: ["ts", "tsx", "js", "jsx", "json", "node"],
+  transform: {
+    "^.+\\.(ts|tsx)$": "ts-jest",
+  },
+  transformIgnorePatterns: ["/node_modules/"],
 };
+
+export default config;
